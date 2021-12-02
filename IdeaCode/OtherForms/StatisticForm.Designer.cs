7ed109d6aa50fc1857c7575ab9412fda96455457
1KitCat1 <denischool119@gmail.com>
2021-12-03 00:17:58 +0200

    Statistic second ready

--- IdeaCode/OtherForms/StatisticForm.Designer.cs
+++ IdeaCode/OtherForms/StatisticForm.Designer.cs
@@ -29,32 +29,40 @@ namespace IdeaCode.OtherForms
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chartCountProblemsByComplexity = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chartUsersByRating = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chartCountProblemsByComplexity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartUsersByRating)).BeginInit();
             this.SuspendLayout();
             // 
             // chartCountProblemsByComplexity
             // 
             this.chartCountProblemsByComplexity.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
             this.chartCountProblemsByComplexity.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(140)))), ((int)(((byte)(235)))));
-            chartArea2.Name = "ChartArea1";
-            this.chartCountProblemsByComplexity.ChartAreas.Add(chartArea2);
-            legend2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            legend2.IsTextAutoFit = false;
-            legend2.Name = "Legend1";
-            this.chartCountProblemsByComplexity.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.chartCountProblemsByComplexity.ChartAreas.Add(chartArea1);
+            legend1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            legend1.IsTextAutoFit = false;
+            legend1.Name = "Legend1";
+            this.chartCountProblemsByComplexity.Legends.Add(legend1);
             this.chartCountProblemsByComplexity.Location = new System.Drawing.Point(113, 80);
             this.chartCountProblemsByComplexity.Name = "chartCountProblemsByComplexity";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series2.LabelForeColor = System.Drawing.Color.Transparent;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chartCountProblemsByComplexity.Series.Add(series2);
+            this.chartCountProblemsByComplexity.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.LabelForeColor = System.Drawing.Color.Transparent;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartCountProblemsByComplexity.Series.Add(series1);
             this.chartCountProblemsByComplexity.Size = new System.Drawing.Size(600, 318);
             this.chartCountProblemsByComplexity.TabIndex = 0;
             this.chartCountProblemsByComplexity.Text = "chart1";
@@ -71,6 +79,52 @@ namespace IdeaCode.OtherForms
             this.label1.TabIndex = 1;
             this.label1.Text = "Problems Complexity";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.label2.Location = new System.Drawing.Point(84, 947);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Problems Complexity";
+            // 
+            // chartUsersByRating
+            // 
+            this.chartUsersByRating.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.chartUsersByRating.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(140)))), ((int)(((byte)(235)))));
+            chartArea2.Name = "ChartArea1";
+            this.chartUsersByRating.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            legend2.IsTextAutoFit = false;
+            legend2.Name = "Legend1";
+            this.chartUsersByRating.Legends.Add(legend2);
+            this.chartUsersByRating.Location = new System.Drawing.Point(113, 544);
+            this.chartUsersByRating.Name = "chartUsersByRating";
+            this.chartUsersByRating.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series2.LabelForeColor = System.Drawing.Color.Transparent;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartUsersByRating.Series.Add(series2);
+            this.chartUsersByRating.Size = new System.Drawing.Size(600, 318);
+            this.chartUsersByRating.TabIndex = 3;
+            this.chartUsersByRating.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.label3.Location = new System.Drawing.Point(147, 499);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "UsersByRating";
+            // 
             // StatisticForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -78,11 +132,15 @@ namespace IdeaCode.OtherForms
             this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(20)))), ((int)(((byte)(90)))));
             this.ClientSize = new System.Drawing.Size(1156, 538);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chartUsersByRating);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.chartCountProblemsByComplexity);
             this.Name = "StatisticForm";
             this.Text = "Statistic";
             ((System.ComponentModel.ISupportInitialize)(this.chartCountProblemsByComplexity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartUsersByRating)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,5 +150,8 @@ namespace IdeaCode.OtherForms
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chartCountProblemsByComplexity;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartUsersByRating;
+        private System.Windows.Forms.Label label3;
     }
 }
--- IdeaCode/OtherForms/StatisticForm.cs
+++ IdeaCode/OtherForms/StatisticForm.cs
@@ -52,6 +52,39 @@ namespace IdeaCode.OtherForms
             chartCountProblemsByComplexity.Series["Series1"].Points.AddXY("2501-3000", countTaskComplexity[5]);
             chartCountProblemsByComplexity.Series["Series1"].Points.AddXY("3001-3500", countTaskComplexity[6]);
 
+            // Users by rating
+
+            var countUsersByRating = new List<int>();
+            for (int start = 1; start + 399 <= 3600; start += 400)
+            {
+                int end = start + 399;
+                int count = 0;
+                using (SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-34VCO73\SQLEXPRESS;Initial Catalog=IdeaCode;Integrated Security=True"))
+                {
+                    conn.Open();
+                    string request = "SELECT count(id_user) FROM Users WHERE rating>=" + start.ToString() +
+                        " AND rating<=" + end.ToString();
+                    SqlDataAdapter sda = new SqlDataAdapter(request, conn);
+                    var dt = new DataTable();
+                    try
+                    {
+                        sda.Fill(dt);
+                        DataRow dr = dt.Rows[0];
+                        count = (int)dr[0];
+                    }
+                    catch
+                    {
+                        count = 0;
+                        //maybe some error;
+                    }
+                    countUsersByRating.Add(count);
+                    conn.Close();
+                }
+            }
+            for(int end = 400; end <= 3600; end += 400)
+            {
+                chartUsersByRating.Series["Series1"].Points.AddXY((end-399).ToString() + "-" + (end.ToString()), countUsersByRating[(int)(end/400)-1]);
+            }
         }
 
         private void chartCountProblemsByComplexity_Click(object sender, EventArgs e)
